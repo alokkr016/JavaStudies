71b0e8082aa420d97af231d3429290ebd28d6143
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2021-03-16 21:28:15 +0530

    Week 5

--- src/kumar/alok/Fibonacci.java
+++ src/kumar/alok/Fibonacci.java
@@ -4,8 +4,7 @@ public class Fibonacci {
 
 
     public static void main(String[] args) {
-        int n;
-        n = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[0]);
         int fibo1 = 0, fibo2 = 1;
         System.out.print(fibo1 + " ");
         while (n > 1){
